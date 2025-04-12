8ca36ddbf9f487a090d5c6bc9c093b7ebcd23e56
AstikVishnoi <astikvishnoi15@gmail.com>
2025-04-12 09:48:50 +0000

    program.cs and json and feedback

--- dotnetapp/Controllers/FeedbackController.cs
+++ dotnetapp/Controllers/FeedbackController.cs
@@ -9,7 +9,44 @@ namespace dotnetapp.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class FeedbackController : ControllerBase
-    {
-        
+    {   
+        private readonly ApplicationDbContext _context;
+        public FeedbackService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Retrieves all feedbacks
+        public async Task<IEnumerable<Feedback>> GetAllFeedbacks()
+        {
+            return await _context.Feedbacks.ToListAsync();
+        }
+
+        // Retrieves feedbacks by UserId
+        public async Task<IEnumerable<Feedback>> GetFeedbacksByUserId(int userId)
+        {
+            return await _context.Feedbacks.Where(f => f.UserId == userId).ToListAsync();
+        }
+
+        // Adds new feedback
+        public async Task<bool> AddFeedback(Feedback feedback)
+        {
+            _context.Feedbacks.Add(feedback);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        // Deletes feedback by FeedbackId
+        public async Task<bool> DeleteFeedback(int feedbackId)
+        {
+            var feedback = await _context.Feedbacks.FindAsync(feedbackId);
+
+            if (feedback == null)
+            {
+                return false;
+            }
+
+            _context.Feedbacks.Remove(feedback);
+            return await _context.SaveChangesAsync() > 0;
+        }
     }
 }
