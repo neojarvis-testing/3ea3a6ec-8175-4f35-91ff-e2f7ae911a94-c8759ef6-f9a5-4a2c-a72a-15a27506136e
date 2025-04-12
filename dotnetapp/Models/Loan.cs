1997a2009f76c59ab5f3aa4b49c4d76c39dae050
Keerthana20116 <keer20116.cb@rmkec.ac.in>
2025-04-12 09:32:17 +0000

    3 models

--- dotnetapp/Models/Loan.cs
+++ dotnetapp/Models/Loan.cs
@@ -7,6 +7,13 @@ namespace dotnetapp.Models
 {
     public class Loan
     {
-        
+        public int LoanId { get; set; }  
+        public string LoanType { get; set; }  
+        public string Description { get; set; }  
+        public decimal InterestRate { get; set; } 
+        public decimal MaximumAmount { get; set; }  
+        public int RepaymentTenure { get; set; }  
+        public string Eligibility { get; set; }  
+        public string DocumentsRequired { get; set; }
     }
 }
--- dotnetapp/Models/LoanApplication.cs
+++ dotnetapp/Models/LoanApplication.cs
@@ -7,6 +7,17 @@ namespace dotnetapp.Models
 {
     public class LoanApplication
     {
-        
+        public int LoanApplicationId { get; set; }  
+        public int UserId { get; set; }  
+        public User? User { get; set; }  
+        public int LoanId { get; set; }  
+        public Loan? Loan { get; set; }  
+        public DateTime SubmissionDate { get; set; }  
+        public int LoanStatus { get; set; }  
+        public string FarmLocation { get; set; }  
+        public string FarmerAddress { get; set; }  
+        public decimal FarmSizeInAcres { get; set; }  
+        public string FarmPurpose { get; set; }  
+        public string File { get; set; } 
     }
 }
