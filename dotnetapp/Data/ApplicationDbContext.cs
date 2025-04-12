1ac8a7ecef3f059c808185b328f8ecb0f695978c
snehalg1104 <snehalgaikwad7273@gmail.com>
2025-04-12 06:30:23 +0000

    added data folder application db context and model folder 4 models

---
+++ dotnetapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetapp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<LoanApplication> LoanApplications { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+    }
+}
---
+++ dotnetapp/Models/Feedback.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetapp.Models
+{
+    public class Feedback
+    {
+
+        public int Feedbackld { get; set; }
+        public int Userld { get; set; }
+        public User? User { get; set; }
+        public string FeedbackText { get; set; }
+        public DateTime Date { get; set; }
+        
+    }
+}
+
+
