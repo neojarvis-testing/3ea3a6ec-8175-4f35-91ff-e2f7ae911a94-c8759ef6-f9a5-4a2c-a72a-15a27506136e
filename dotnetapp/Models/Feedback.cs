258256ac487b24d57eff04ab521a8d2836bbd251
snehalg1104 <snehalgaikwad7273@gmail.com>
2025-04-12 09:19:35 +0000

    4 Models

--- dotnetapp/Models/Feedback.cs
+++ dotnetapp/Models/Feedback.cs
@@ -7,6 +7,12 @@ namespace dotnetapp.Models
 {
     public class Feedback
     {
+
+        public int Feedbackld { get; set; }
+        public int Userld { get; set; }
+        public User? User { get; set; }
+        public string FeedbackText { get; set; }
+        public DateTime Date { get; set; }
         
     }
 }
