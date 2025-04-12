72985c7665cf1830b7d97b26a9fb154658c263e5
Astik Vishnoi <76203859+AstikVishnoi@users.noreply.github.com>
2025-04-12 14:03:07 +0530

    Update LoanController.cs

--- dotnetapp/Controllers/LoanController.cs
+++ dotnetapp/Controllers/LoanController.cs
@@ -10,10 +10,6 @@ namespace dotnetapp.Controllers
     [Route("api/[controller]")]
     public class LoanController : ControllerBase
     {
-<<<<<<< HEAD
-        //private readonly ApplicationDbContext db;
-=======
->>>>>>> f9aaf8181901cf298b8c15c7ff61e1d53c7970a5
-        
+    
     }
 }
