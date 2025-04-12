791935b8d984f050c82b81fbb6382618c8c29a86
snehalg1104 <snehalgaikwad7273@gmail.com>
2025-04-12 08:30:13 +0000

    ApplicationDbContext

--- dotnetapp/Data/ApplicationDbContext.cs
+++ dotnetapp/Data/ApplicationDbContext.cs
@@ -2,11 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnetapp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnetapp.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<LoanApplication> LoanApplications { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        
         
     }
 }
+
+
