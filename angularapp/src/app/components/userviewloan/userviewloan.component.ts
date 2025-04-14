04acd48a93495be66fbd88ce8731f9989596d7d7
Keerthana20116 <keer20116.cb@rmkec.ac.in>
2025-04-14 10:01:23 +0000

    3 components

--- angularapp/src/app/components/userviewloan/userviewloan.component.ts
+++ angularapp/src/app/components/userviewloan/userviewloan.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoanService } from '../services/loan.service';
+ 
 @Component({
-  selector: 'app-userviewloan',
-  templateUrl: './userviewloan.component.html',
-  styleUrls: ['./userviewloan.component.css']
+  selector: 'app-user-view-loan',
+  templateUrl: './user-view-loan.component.html',
+  styleUrls: ['./user-view-loan.component.css']
 })
-export class UserviewloanComponent implements OnInit {
-
-  constructor() { }
-
+export class UserViewLoanComponent implements OnInit {
+  loans: any[] = [];
+  appliedLoans: number[] = []; // store IDs of applied loans
+  isLoading = true;
+ 
+  constructor(private loanService: LoanService) {}
+ 
   ngOnInit(): void {
+    this.loadLoans();
   }
-
+ 
+  loadLoans() {
+    this.loanService.getLoans().subscribe(
+      (data: any[]) => {
+this.loans = data;
+        this.appliedLoans = this.loanService.getUserAppliedLoanIds();
+        this.isLoading = false;
+      },
+      error => {
+        this.isLoading = false;
+this.loans = [];
+      }
+    );
+  }
+ 
+  hasApplied(loanId: number): boolean {
+    return this.appliedLoans.includes(loanId);
+  }
+ 
+  applyForLoan(loanId: number) {
+    this.loanService.applyLoan(loanId).subscribe(response => {
+      alert('Successfully Applied');
+      this.appliedLoans.push(loanId);
+    });
+  }
 }
